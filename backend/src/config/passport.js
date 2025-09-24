e7dc587760c15fa8c6dc8da59dd338b5b87d272d
akshat770 <asingh77707@gmail.com>
2025-09-25 03:45:33 +0530

    refactor: update User model import path and change export to ES module syntax

--- backend/src/config/passport.js
+++ backend/src/config/passport.js
@@ -1,6 +1,7 @@
+
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
-const User = require('./models/User');
+const User = require('../models/User.js');
 
 passport.use(new GoogleStrategy({
   clientID: process.env.GOOGLE_CLIENT_ID,
