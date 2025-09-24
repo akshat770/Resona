2cba6902dffdb23b5aea6701bd11d42a8c6e9347
akshat770 <asingh77707@gmail.com>
2025-09-25 04:06:01 +0530

    fix: correct middleware import path in Spotify routes

--- backend/src/routes/spotify.js
+++ backend/src/routes/spotify.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const auth = require('../middleware/auth.js');
+const auth = require('../middlewares/auth.js');
 const User = require('../models/User.js');
 const axios = require('axios');
 
